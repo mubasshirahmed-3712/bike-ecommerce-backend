0f8e35258b23a1cb22d17c47a28ed87ccbf461a7
Mubasshir Ahmed <mubasshir3712@gmail.com>
2025-03-19 21:17:00 +0530

    final update done

--- models/productModel.js
+++ models/productModel.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  brand: { type: String, required: true }, // ✅ Added brand
+  category: { type: String, required: true }, // ✅ Added category
+  engineCapacity: { type: Number, required: true }, // ✅ Added engine capacity
   description: { type: String, required: true },
   image: { type: String, required: true },
   price: { type: Number, required: true },
